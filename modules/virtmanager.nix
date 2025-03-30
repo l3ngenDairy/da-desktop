12ad68bed8dbd3d3c0ae9e48ec7780568a215c3b
l3ngenDairy <davidhobman@protonmail.com>
2025-03-30 18:35:14 +1300

    virt autostart network

--- modules/virtmanager.nix
+++ modules/virtmanager.nix
@@ -1,11 +1,29 @@
-{ pkgs , ... }: {
+{ pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     libvirt
     qemu
     spice-vdagent
     virt-manager 
-  ];      
-  virtualisation.libvirtd.enable = true;
+  ];
+  
+  virtualisation.libvirtd = {
+    enable = true;
+    onBoot = "start";
+    onShutdown = "suspend";
+    qemu = {
+      ovmf.enable = true;
+      runAsRoot = true;
+    };
+    
+    # Ensure the default network is enabled and auto-started
+    networks = {
+      default = {
+        enable = true;
+        autoStart = true;
+      };
+    };
+  };
+  
   programs.virt-manager = {
     enable = true;
     package = pkgs.virt-manager;
